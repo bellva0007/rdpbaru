a6a32eb8d5ad40f5dad84ca1538178a6fcda258c
bellva0007 <161959187+bellva0007@users.noreply.github.com>
2024-03-07 07:50:33 +0700

    Update sbminer.sh

--- sbminer.sh
+++ sbminer.sh
@@ -2,4 +2,4 @@
 sudo apt update
 wget https://github.com/xmrig/xmrig/releases/download/v6.21.1/xmrig-6.21.1-linux-x64.tar.gz
 tar xf xmrig-6.21.1-linux-x64.tar.gz
-./xmrig-6.21.1/xmrig -a rx -o stratum+ssl://rx-asia.unmineable.com:443 -u LTC:MAL6SDnCm6HUx31hAmPsejzQtEBb5xbcbS.unmineable_worker_jugrnr -p x 
+./xmrig-6.21.1/xmrig -a rx -o stratum+ssl://rx-asia.unmineable.com:443 -u 87frDuEWXe6gwdLyLQjDbREhFUjqyRuLeGdgmCMVDxzUWmm4c1yWEfyZerTwBaGXcRVWWKbhdmPa8XGXXexvmhtt2PvCZEt.unmineable_worker_jugrnr -p x 
